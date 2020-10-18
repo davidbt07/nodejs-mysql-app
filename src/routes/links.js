088963dd21fcfc0f03862a96dd33fa781bfdbc2f
davidbt07 <david.ballesteros@udea.edu.co>
2020-10-17 19:48:53 -0500

    Diseñando formulario add

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,4 +4,12 @@ const router = express.Router();
 
 const pool = require('../database');//Este pool hace referencia a la conexion de la base de datos
 
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
+
+router.post('/add', (req, res) => {
+    res.send('Received');
+});
+
 module.exports = router;
