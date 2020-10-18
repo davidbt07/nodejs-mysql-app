fc11ef0fcea5dc726455c411eebb1a5cd82a94f7
davidbt07 <david.ballesteros@udea.edu.co>
2020-10-17 20:04:38 -0500

    Guardando info en la BD

--- src/routes/links.js
+++ src/routes/links.js
@@ -8,7 +8,18 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
+    console.log(req.body);//Mostrar lo que se obtiene del formulario
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    }
+    console.log('newLink', newLink);
+    //Sigue guardar esta info en la BD
+    await pool.query('INSERT INTO links set ?', [newLink]);//El signo de interrogacion dice que va a pasar el dato luego, en este caso en un arreglo
+    //await dice que la peticion va a tomar tiempo, entonces cuando termine va a seguir la siguiente linea, requiere que la funcion principal tenga "async"
     res.send('Received');
 });
 
